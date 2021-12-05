87266c57b81d47fad32635ea791c2422174ca682
Denise Nanni <denise@localhost.localdomain>
2021-12-05 20:26:31 +0100

    home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import Menu from "./Menu.js";
 import { isMobile } from "react-device-detect";
-import './Header.scss';
+import "./Header.scss";
+import { Link } from "react-router-dom";
 
 function Header() {
   const name = "NAME";
   return (
     <div className={`header ${isMobile ? "mobile" : ""}`}>
-      <div className="name-section">{name}</div>
+      <Link to={`/`}>
+        <div className="name-section">{name}</div>
+      </Link>
       <div className="menu-section">
         <Menu></Menu>
       </div>
